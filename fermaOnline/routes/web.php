b4e10ef9cce29425f631de686cb5658339194555
Arber Shahini <arber.shahini2003@gmail.com>
2024-04-29 18:15:04 +0200

    added routes

--- fermaOnline/routes/web.php
+++ fermaOnline/routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SignUpController;
+use App\Http\Middleware\CheckIfRouteExists;
 use Illuminate\Support\Facades\Route;
+use app\Http\Middleware\URLRedirect;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [LoginController::class, 'show'])->name('emptyUrl');
+
+Route::get('/login', [LoginController::class, 'show'])->name('login');
+
+Route::get('/profile/{guid_id}', [ProfileController::class, 'show'])->name('profile');
+
+Route::get('/logout', [ProfileController::class, 'logout'])->name('logout');
+
+Route::get('/signup', [SignUpController::class, 'show'])->name('signup');
+
+Route::post('/signup-post', [SignUpController::class, 'signupPost'])->name('signup-post');
+
+Route::post('/login-post', [LoginController::class, 'loginPost'])->name('login-post');
+
+Route::get('/profile/{guid_id}/changePass', [ProfileController::class, 'showChangePassword'])->name('showChangePassword');
+
+Route::post('/profile/{guid_id}/changePass-post', [ProfileController::class, 'changePassword'])->name('changePassword');
+
+
+
+
+
