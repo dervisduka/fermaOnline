c5c6dcb0f58f938dad8788ec741096f3d4ca09d0
Dervis Duka <dervis.duka@fshnstudent.info>
2024-05-06 23:07:33 +0200

    add creation of product and modification of it

--- fermaOnline/app/Http/Controllers/MainPageController.php
+++ fermaOnline/app/Http/Controllers/MainPageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Perdorues;
+use App\Models\Produkt;
 use Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Hash;
@@ -12,14 +13,39 @@ class MainPageController extends Controller
 {
     public function show(string $guid_id){
         $user = Perdorues::where('guid_id', $guid_id)->firstOrFail();
+        $produkt = Produkt::all();
         $data = [
             'guid_id' => $guid_id,
             'username' => $user->username,
             'email' => $user->email,
             'is_admin' => $user->is_admin,
+            'produkte' => $produkt,
         ];
         return view('mainPage', ['data' => $data]);
     }
+
+    public function updateProduct(Request $request, $guid_id, $product_id)
+    {
+        $validatedData = $request->validate([
+            'stock' => 'required|integer',
+            'price' => 'required|numeric',
+            'is_active' => 'boolean', 
+        ]);
+    
+
+        $product = Produkt::findOrFail($product_id);
+    
+
+        $product->sasia = $validatedData['stock'];
+        $product->cmimi = $validatedData['price'];
+        $product->is_active = $request->has('is_active');
+        $product->save();
+    
+        return redirect()->back()->with('success', 'Product updated successfully');
+    }
+    
+    
+    
 
     public function logout(Request $request){
         Auth::logout();
